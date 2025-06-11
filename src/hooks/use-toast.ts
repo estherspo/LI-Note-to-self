fbffffaa507d28b49bbe884b3756e11fe8044554
Esther Sportello <EstherSpo@gmail.com>
2025-06-11 23:41:38 +0000

    update all toasts to disappear after 3 seconds

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,3 +1,4 @@
+
 "use client"
 
 // Inspired by react-hot-toast library
@@ -9,7 +10,7 @@ import type {
 } from "@/components/ui/toast"
 
 const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
+const TOAST_REMOVE_DELAY = 1000000 // Time in ms to wait before removing the toast from memory after it's been dismissed
 
 type ToasterToast = ToastProps & {
   id: string
@@ -158,6 +159,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
+      duration: 3000, // Set default duration to 3 seconds
       onOpenChange: (open) => {
         if (!open) dismiss()
       },
